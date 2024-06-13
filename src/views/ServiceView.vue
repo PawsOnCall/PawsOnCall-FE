16777e264badfeaab2c3756e0915829c835d1c4e
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-06-13 16:54:32 -0700

    add service page

---
+++ src/views/ServiceView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="service">
+    <h1>This is an Service and Price page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .service {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
