0c5da6377b737dabe025f8158d91265592fca886
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-06-13 16:54:08 -0700

    add groomer page

---
+++ src/views/GroomerView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="groomer">
+    <h1>Service selection</h1>
+    <ul>
+      <li>Select at least one service you're interested in. You can always add morelater..</li>
+      <li>
+        If you select more than one service, you will only see one of them duringthe sign up
+        process.
+      </li>
+      <li>
+        After your profile is submitted for review, you can edit your selectedservices or add more.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .groomer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
