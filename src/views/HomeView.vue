5da69b17cdba76874d9c1168a357bfdd6aea757a
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-06-13 16:53:57 -0700

    Add Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="search">
+    <h1>This is the home page. Search professionals for your pet</h1>
+  </div>
 </template>
+<style>
+@media (min-width: 1024px) {
+  .search {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
