671f65720c3daaba58bf4a54bb23bd9213c87a57
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-06-22 10:51:04 -0700

    add proxy to solve CORS issue

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,21 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 5000,
+    // proxy: to solve CORS issue
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8080', // backend server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
