fd1816f19d2aa2ffe4cc21bdf749ca442b0e45f6
huiminlifdu <h.li1@student.fdu.edu>
2024-06-22 13:20:55 -0700

    update forgot password page

--- src/views/ForgotPasswordView.vue
+++ src/views/ForgotPasswordView.vue
@@ -5,58 +5,63 @@
       To reset your password, type the full email address you used to sign up for PawsOnCall and
       we'll send you an e-mail to walk you through resetting your password.
     </div>
-    <el-form :model="form" label-width="auto" style="max-width: 600px">
+    <el-form :model="form" label-width="auto" style="max-width: 600px" :size="'large'">
       <el-form-item label="Email:">
-        <el-input v-model="form.email" clearable />
+        <el-input v-model="form.email" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit" style="margin-left: 40px">Sumbit</el-button>
+        <el-button type="primary" @click="onSubmit" style="margin-left: 60px">Sumbit</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .forget-page {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    margin: 10px auto;
-    padding: 20px;
-    width: 600px;
-    /* border: 1px solid #000; */
-    background: #f2f2f2;
+.forget-page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 10px auto;
+  padding: 20px;
+  width: 600px;
+  background: #f2f2f2;
 
-    h1 {
-      font-size: 18px;
-      margin: 20px;
-      font-weight: bold;
-    }
-    .form-item {
-      margin: 10px;
-      span {
-        display: inline-block;
-        width: 100px;
-      }
-    }
+  h1 {
+    font-size: 18px;
+    margin: 20px;
+    font-weight: bold;
+  }
 
-    .tips {
-      margin-top: 20px;
-    }
+  .tips {
+    margin-bottom: 20px;
+  }
+
+  .el-form-item {
+    min-width: 210px;
   }
 }
 </style>
 
 <script lang="ts" setup>
 import { reactive } from 'vue'
+import { ElMessage } from 'element-plus'
 
 const form = reactive({
   email: ''
 })
 
 const onSubmit = () => {
-  console.log('submit!')
+  if (!form.email) {
+    ElMessage({
+      type: 'error',
+      message: `Please enter your email address.`
+    })
+  } else {
+    ElMessage({
+      type: 'success',
+      message: `An email has been sent to ${form.email}. Please check your inbox.`
+    })
+  }
 }
 </script>
