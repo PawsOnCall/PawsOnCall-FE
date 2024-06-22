3db193440f8f1bb0c86d3ac323b1f85dd325dd4e
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-06-22 10:51:32 -0700

    update policy page

--- src/views/PolicyView.vue
+++ src/views/PolicyView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="about">
+  <div class="policy">
     <h1>This is an Terms of Service and Privacy Policy page</h1>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .policy {
     min-height: 100vh;
     display: flex;
     align-items: center;
