58d411d1d9f926c4c9fc56ef4941f1117449212f
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-06-28 14:20:59 -0700

    add siderbar & usercard components

---
+++ src/components/Siderbar.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-menu
+    class="el-menu-vertical-demo"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <el-menu-item index="1" @click="onDashbord">
+      <i class="el-icon-menu"></i>
+      <span slot="title">Dashboard</span>
+    </el-menu-item>
+    <el-menu-item index="2" @click="onProfile">
+      <i class="el-icon-user"></i>
+      <span slot="title">Profile</span>
+    </el-menu-item>
+    <el-menu-item index="3" @click="onOrders">
+      <i class="el-icon-message"></i>
+      <span slot="title">Orders</span>
+    </el-menu-item>
+    <el-menu-item index="5">
+      <i class="el-icon-picture"></i>
+      <span slot="title">Photos</span>
+    </el-menu-item>
+    <el-menu-item index="6" @click="onLogOut">
+      <i class="el-icon-switch-button"></i>
+      <span slot="title">Log Out</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+import { useRouter } from 'vue-router'
+import { userAuthStore } from '../stores/userAuthStore'
+const authStore = userAuthStore()
+
+const props = defineProps({
+  isGroomer: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const router = useRouter()
+
+const onDashbord = () => {
+  if (props.isGroomer) {
+    router.push({ name: 'groomer-dashboard' })
+  } else {
+    router.push({ name: 'user-dashboard' })
+  }
+}
+const onProfile = () => {
+  if (props.isGroomer) {
+    router.push({ name: 'groomer-profile' })
+  } else {
+    router.push({ name: 'user-profile' })
+  }
+}
+const onOrders = () => {
+  if (props.isGroomer) {
+    router.push({ name: 'groomer-orders' })
+  } else {
+    router.push({ name: 'user-orders' })
+  }
+}
+
+const onLogOut = () => {
+  authStore.logout()
+  router.push({ name: 'home' })
+}
+</script>
+
+<style scoped>
+.el-menu-vertical-demo {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,96 @@
+<template>
+  <el-card class="box-card">
+    <el-row>
+      <el-col :span="12">
+        <img width="60px" src="@/assets/logo.jpg" class="logo" />
+      </el-col>
+      <el-col :span="12">
+        <h2>Jack</h2>
+        <el-link>Edit Profile</el-link>
+      </el-col>
+    </el-row>
+    <div class="wallet-info">
+      <div>
+        <strong>WALLET</strong>
+        <p>$0.00</p>
+      </div>
+      <div v-if="isGroomer">
+        <strong>UPCOMING EARNINGS</strong>
+        <p>$0.00</p>
+      </div>
+      <div v-if="!isGroomer">
+        <strong>PROCESSING PAYMENTS</strong>
+        <p>$0.00</p>
+        <p>Funds from past bookings begin processing 48 hours after they end.</p>
+      </div>
+      <template v-if="!isGroomer">
+        <div>
+          <el-button type="default" @click="modifyPaymentMethod"
+            >Add or Modify a Payment Method</el-button
+          >
+        </div>
+        <div>
+          <el-button type="default" @click="viewPayments">View Payments</el-button>
+        </div>
+      </template>
+      <template v-else>
+        <div>
+          <el-button type="default" @click="OnReceivePayment"
+            >Manage Methods of Receving Payments
+          </el-button>
+        </div>
+        <div>
+          <el-button type="default" @click="OnviewEarnings">View Earnings</el-button>
+        </div>
+      </template>
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  isGroomer: {
+    type: Boolean,
+    default: false
+  }
+})
+
+// Use useRouter to get the router instance
+const router = useRouter()
+
+// Define methods as functions
+const managePayoutMethods = () => {
+  // Implement method
+}
+
+const modifyPaymentMethod = () => {
+  // Implement method
+}
+
+const viewPayments = () => {
+  // Implement method
+}
+
+const OnReceivePayment = () => {
+  router.push({ name: 'receive-payment' })
+}
+
+const OnviewEarnings = () => {
+  router.push({ name: 'earning-history' })
+}
+</script>
+
+<style scoped>
+.logo {
+  width: 100px;
+}
+.wallet-info {
+  margin-top: 20px;
+}
+.wallet-info > div {
+  margin-bottom: 20px;
+}
+</style>
