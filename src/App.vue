d87bb5d8488a21928e8821b4e5424e152ee3ac92
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-07-01 10:15:30 -0700

    Access the login state from the Pinia store

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { ref } from 'vue'
+import { userAuthStore } from './stores/userAuthStore'
+
 import NavigationBar from './components/NavigationBar.vue'
+// Access the login state from the Pinia store
+const authStore = userAuthStore()
 </script>
 
 <template>
-  <NavigationBar />
+  <NavigationBar v-if="!authStore.isLogin" />
 
   <RouterView />
 </template>
