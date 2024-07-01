cc30eb3c9dd914072831663f6f1f52bc17441656
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-07-01 16:40:05 -0700

    add Groomer Dashboard page

---
+++ src/views/GroomerDashboardView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="dashboard">
+    <el-row :gutter="20">
+      <el-col :span="20">
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <UserCard :isGroomer="true" />
+          </el-col>
+          <el-col :span="16"> </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="4">
+        <Sidebar :isGroomer="true" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from '@/components/Siderbar.vue'
+import UserCard from '@/components/UserCard.vue'
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import { ElMessage } from 'element-plus'
+
+const router = useRouter()
+</script>
+
+<style scoped>
+.dashboard {
+  padding: 20px;
+  background: #f2f2f2;
+}
+.user-card {
+  margin-bottom: 20px;
+}
+
+.refer {
+  padding: 20px;
+  border: 1px solid #ebeef5;
+  border-radius: 4px;
+  background: #fff;
+  margin-bottom: 20px;
+}
+.pets-section {
+  padding: 20px;
+  border: 1px solid #ebeef5;
+  border-radius: 4px;
+  background: #fff;
+}
+.add-pet-card {
+  text-align: center;
+  border: 2px dashed #dcdfe6;
+  padding: 20px;
+}
+.add-pet {
+  cursor: pointer;
+}
+.add-pet i {
+  font-size: 40px;
+}
+</style>
