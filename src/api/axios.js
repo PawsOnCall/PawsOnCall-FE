10d34580c63443c04427aa59bf62a6a3aaa592bf
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-07-01 16:40:25 -0700

    upate axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,18 +1,19 @@
 // src/axios.js
 import axios from 'axios'
 
+// 创建一个 Axios 实例
 const instance = axios.create({
-  baseURL: '/',
-  timeout: 1000,
+  baseURL: '/', // 替换为你的 API 基础 URL
+  timeout: 1000, // 请求超时
   headers: {
     'Content-Type': 'application/json'
   }
 })
 
-// add Authorization header to all requests
+// 添加请求拦截器，在每次请求前自动添加 Authorization 头
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token') // 假设 token 存储在 localStorage 中
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
