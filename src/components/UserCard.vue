f226772a236ed453014598a9d9bcd7d1f08e8be7
Xingfeng Wang <x.wang3@student.fdu.edu>
2024-07-03 00:03:27 -0700

    update user dashboard page, integrate backend api

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -2,38 +2,23 @@
   <el-card class="box-card">
     <el-row>
       <el-col :span="12">
-        <img width="60px" src="@/assets/logo.jpg" class="logo" />
+        <img width="60px" :src="`${userInfo.photo}`" class="logo" />
       </el-col>
       <el-col :span="12">
-        <h2>Jack</h2>
-        <el-link>Edit Profile</el-link>
+        <h2>{{ userInfo.name }}</h2>
+        <el-link @click="OnEditProfile">Edit Profile</el-link>
       </el-col>
     </el-row>
     <div class="wallet-info">
-      <div>
-        <strong>WALLET</strong>
-        <p>$0.00</p>
-      </div>
-      <div v-if="isGroomer">
-        <strong>UPCOMING EARNINGS</strong>
-        <p>$0.00</p>
-      </div>
-      <div v-if="!isGroomer">
-        <strong>PROCESSING PAYMENTS</strong>
-        <p>$0.00</p>
-        <p>Funds from past bookings begin processing 48 hours after they end.</p>
-      </div>
-      <template v-if="!isGroomer">
+      <template v-if="isGroomer">
         <div>
-          <el-button type="default" @click="modifyPaymentMethod"
-            >Add or Modify a Payment Method</el-button
-          >
+          <strong>WALLET</strong>
+          <span>$0.00</span>
         </div>
         <div>
-          <el-button type="default" @click="viewPayments">View Payments</el-button>
+          <strong>UPCOMING EARNINGS</strong>
+          <span>$0.00</span>
         </div>
-      </template>
-      <template v-else>
         <div>
           <el-button type="default" @click="OnReceivePayment"
             >Manage Methods of Receving Payments
@@ -41,6 +26,21 @@
         </div>
         <div>
           <el-button type="default" @click="OnviewEarnings">View Earnings</el-button>
+        </div>
+      </template>
+
+      <template v-else>
+        <div>
+          <p class="payment-amount"><strong>PROCESSING PAYMENTS</strong><span>$0.00</span></p>
+          <p class="tips">Funds from past bookings begin processing 48 hours after they end.</p>
+        </div>
+        <div>
+          <el-button type="default" @click="modifyPaymentMethod"
+            >Add or Modify a Payment Method</el-button
+          >
+        </div>
+        <div>
+          <el-button type="default" @click="viewPayments">View Payments</el-button>
         </div>
       </template>
     </div>
@@ -55,23 +55,28 @@ const props = defineProps({
   isGroomer: {
     type: Boolean,
     default: false
+  },
+  userInfo: {
+    type: Object,
+    default: () => {
+      return {
+        name: 'Frank',
+        photo:
+          'https://www.rawpixel.com/image/6196752/png-sticker-journal#eyJrZXlzIjoiY2F0Iiwic29ydGVkS2V5cyI6ImNhdCJ9'
+      }
+    }
   }
 })
 
 // Use useRouter to get the router instance
 const router = useRouter()
 
-// Define methods as functions
-const managePayoutMethods = () => {
-  // Implement method
-}
-
 const modifyPaymentMethod = () => {
-  // Implement method
+  router.push({ name: 'user-payment' })
 }
 
 const viewPayments = () => {
-  // Implement method
+  router.push({ name: 'payment-history' })
 }
 
 const OnReceivePayment = () => {
@@ -80,6 +85,10 @@ const OnReceivePayment = () => {
 
 const OnviewEarnings = () => {
   router.push({ name: 'earning-history' })
+}
+
+const OnEditProfile = () => {
+  router.push({ name: 'user-profile' })
 }
 </script>
 
@@ -92,5 +101,15 @@ const OnviewEarnings = () => {
 }
 .wallet-info > div {
   margin-bottom: 20px;
+}
+.tips {
+  color: #999;
+  font-size: 12px;
+}
+.payment-amount {
+  margin-bottom: 10px;
+  strong {
+    margin-right: 10px;
+  }
 }
 </style>
--- src/views/UserDashboardView.vue
+++ src/views/UserDashboardView.vue
@@ -4,19 +4,26 @@
       <el-col :span="20">
         <el-row :gutter="20">
           <el-col :span="8">
-            <UserCard />
+            <UserCard :user-info="{ name: userDashboard.name, photo: userDashboard.photo }" />
           </el-col>
           <el-col :span="16">
-            <div class="refer">
+            <!-- <div class="refer">
               <h2>Refer a friend, Get $20</h2>
               <p>
                 For every friend that books their first grooming, we will give you a $20 credit
                 towards your next booking.
               </p>
               <el-button type="default">Invite a friend</el-button>
-            </div>
+            </div> -->
             <div class="pets-section">
               <h2>Your Pets</h2>
+              <template v-if="userDashboard.pets.length > 1">
+                <div class="petcard" v-for="(pet, i) in userDashboard.pets" :key="i">
+                  <div class="pet-name">Pet Name:{{ pet.name }}</div>
+                  <img class="pet-photo" :src="pet.photo" />
+                </div>
+              </template>
+
               <p>Add your pets or edit their info</p>
               <el-card class="add-pet-card">
                 <div class="add-pet" @click="onAddPet">
@@ -42,6 +49,8 @@ import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import axios from 'axios'
+import { userAuthStore } from '@/stores/userAuthStore'
+
 const router = useRouter()
 
 const onAddPet = () => {
@@ -49,17 +58,57 @@ const onAddPet = () => {
   router.push({ name: 'add-pet' })
 }
 
-const getUser = async function () {
+const userId = userAuthStore().userInfo.userId
+console.log(userId)
+
+const userProfile = reactive({
+  name: 'Frank',
+  email: '',
+  addressLine1: '',
+  addressLine2: '',
+  city: '',
+  country: '',
+  photo: '',
+  phone: '',
+  postCode: '',
+  province: '',
+  emeContactName: '',
+  emeContactPhone: '',
+  userId: ''
+})
+const userDashboard = reactive({
+  userId: '',
+  pets: [],
+  balance: 0,
+  firstName: '',
+  lastName: '',
+  name: '',
+  photo: ''
+})
+const getUserDashboard = async function () {
   try {
-    // the first /api is the proxy in vue.config.js
-    fetch('/api/api/logged-user').then((response) => {
+    axios.get(`/api/api/customer/dashboard?userId=${102}`).then((response) => {
+      // TODO: userId
+      // const userId = userAuthStore().userInfo.userId
+      // axios.get(`/api/api/customer/dashboard?userId=${userId}`).then((response) => {
       console.log(response.data)
+
+      userDashboard.pets = response.data.data.pets
+      userDashboard.balance = response.data.data.balance
+      userDashboard.firstName = response.data.data.firstName
+      userDashboard.photo = response.data.data.photo
+
+      userDashboard.lastName = response.data.data.lastName
+
+      userDashboard.name = response.data.data.firstName + ' ' + response.data.data.lastName
+
+      localStorage.setItem('userDashboard', JSON.stringify(userDashboard))
     })
   } catch (error) {
     console.error(error)
   }
 }
-getUser()
+getUserDashboard()
 </script>
 
 <style scoped>
@@ -94,5 +143,21 @@ getUser()
 }
 .add-pet i {
   font-size: 40px;
+}
+
+.petcard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+  border: 1px solid #ebeef5;
+  padding: 12px;
+
+  .pet-photo {
+    display: block;
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+  }
 }
 </style>
